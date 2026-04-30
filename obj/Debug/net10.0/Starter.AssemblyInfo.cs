c208a9684c76fa90070518e1a9238b412a196ed2
Jerry <hoanghunganh552@gmail.com>
2026-04-30 19:18:04 +0700

    Xóa code cũ của main ra khỏi nhánh mới

--- obj/Debug/net10.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net10.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eecae9b2d7c8a199565cdc7cacfb71aa93b533b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
